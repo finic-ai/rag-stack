105d788646bbdfc8027e1fd83e42c4524063f5e6
ambardhesi <ambar.dhesi@gmail.com>
2023-07-31 14:52:37 -0400

    feat: add file previews

--- server/models/models.py
+++ server/models/models.py
@@ -11,23 +11,29 @@ class Document(BaseModel):
     content: str
     uri: Optional[str] = None
 
+
 class AppConfig(BaseModel):
     app_id: str
     user_id: str
 
 
 class VectorStore(BaseModel, ABC):
-
     @abstractmethod
     async def upsert(self, documents: List[Document]) -> bool:
         pass
-    
+
     @abstractmethod
     async def query(self, query: str) -> List[Document]:
         pass
 
-class LLM(BaseModel, ABC):
 
+class LLM(BaseModel, ABC):
     @abstractmethod
     def ask(self, documents: List[str], question: str) -> List[List[float]]:
         pass
+
+
+class FilePreview(BaseModel):
+    file_name: str
+    # Base64 encoded img content
+    file_preview_img: str
