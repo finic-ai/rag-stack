e88752630344965311b2c5b78081b6218933b25c
hsm207 <hsm207@users.noreply.github.com>
2023-09-11 09:57:39 +0000

    add variables to store weaviate api key

--- scripts/aws/vars.tf
+++ scripts/aws/vars.tf
@@ -13,3 +13,13 @@ variable "model" {
 variable "hf_api_token" {
   type = string
 }
+
+variable "weaviate_authentication_apikey_allowed_keys" {
+  type = string
+  default = "jane-secret-key"
+}
+
+variable "weaviate_authentication_apikey_users" {
+  type = string
+  default = "jane@doe.com"
+}
